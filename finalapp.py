580dc2553160006c2f0ebe39627444e1a3b395b3
Akhil Tony <72643926+Akhil-Tony@users.noreply.github.com>
2021-06-26 12:16:46 +0530

    Update finalapp.py
    if file not none

--- finalapp.py
+++ finalapp.py
@@ -24,58 +24,59 @@ Among popular apps WhatsApp is a free messaging application. According to [Whats
 st.markdown('Analysing WhatsApp chat can give us many surprising insights')
 
 file=st.file_uploader('select your chat.txt file',type=['txt'])
-raw_text=io.TextIOWrapper(file,encoding='utf-8')
-chats=raw_text.readlines()
+if file!=None:
+ raw_text=io.TextIOWrapper(file,encoding='utf-8')
+ chats=raw_text.readlines()
 
-df=help.list_to_DF(chats)
-df=help.data_preperation(df)
+ df=help.list_to_DF(chats)
+ df=help.data_preperation(df)
 
 
-col1,col2=st.beta_columns([1.5,1])
+ col1,col2=st.beta_columns([1.5,1])
 
-with col1:
-    st.write(help.linepolar(df))
-with col2:
-    st.write(help.histogram(df))
+ with col1:
+     st.write(help.linepolar(df))
+ with col2:
+     st.write(help.histogram(df))
 
-col1,col2=st.beta_columns([.3,1])
-with col1:
-    st.markdown('Its funny that i was wondering whether these emojis had any sentimental feelings')
-with col2:
-    st.write(help.treemap(df))
+ col1,col2=st.beta_columns([.3,1])
+ with col1:
+     st.markdown('Its funny that i was wondering whether these emojis had any sentimental feelings')
+ with col2:
+     st.write(help.treemap(df))
 
-st.subheader('Calender Plot')
-st.markdown('This Calender talks the intensity of the chat conversation in each day of the year by month')
-st.write(help.calmapp(df))
+ st.subheader('Calender Plot')
+ st.markdown('This Calender talks the intensity of the chat conversation in each day of the year by month')
+ st.write(help.calmapp(df))
 
-st.subheader('Most Frequent Word Plot')
-st.markdown('Even i have no idea on what sense these most frequently used words can give you about your chat conversation')
-st.write(help.plott(help.get_wordcloud(df)))
+ st.subheader('Most Frequent Word Plot')
+ st.markdown('Even i have no idea on what sense these most frequently used words can give you about your chat conversation')
+ st.write(help.plott(help.get_wordcloud(df)))
 
-st.subheader('WorkFlow')
-st.image(end_img)
+ st.subheader('WorkFlow')
+ st.image(end_img)
 
-with st.beta_expander('About'):
-    col1,col2,col3=st.beta_columns(3)
+ with st.beta_expander('About'):
+     col1,col2,col3=st.beta_columns(3)
 
-    with col1:
-        st.subheader('Developer Contact')
-        st.markdown('we love to hear your feedback or incase any suggestions, mail me at akhi_mwon@gmail.com')
+     with col1:
+         st.subheader('Developer Contact')
+         st.markdown('we love to hear your feedback or incase any suggestions, mail me at akhi_mwon@gmail.com')
 
-        st.subheader('info')
-        st.markdown('released on 22 june 2021')
+         st.subheader('info')
+         st.markdown('released on 22 june 2021')
 
-        st.subheader('App Permission')
-        st.markdown('WhatsApp Chat Analysis may require permission to read your device internal storage')
+         st.subheader('App Permission')
+         st.markdown('WhatsApp Chat Analysis may require permission to read your device internal storage')
 
-    with col2:
+     with col2:
 
-        st.subheader('a_t logics')
-        st.markdown('a_t logics is a non profitable company owned and maintained by Mr.Akhil Tony')
+         st.subheader('a_t logics')
+         st.markdown('a_t logics is a non profitable company owned and maintained by Mr.Akhil Tony')
 
-        st.subheader('Licence')
-        st.markdown('a_t logics is currently running as a non recognized software company')
+         st.subheader('Licence')
+         st.markdown('a_t logics is currently running as a non recognized software company')
 
-    with col3:
-        st.subheader('Acknowledgements')
-        st.markdown('Thanks to my dearest friend Sona Martin for her help and support')
+     with col3:
+         st.subheader('Acknowledgements')
+         st.markdown('Thanks to my dearest friend Sona Martin for her help and support')
