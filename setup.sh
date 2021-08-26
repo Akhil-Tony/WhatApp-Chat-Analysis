ec601a4a4f9dfda53abea40d4dc6275d6978686e
Akhil Tony <72643926+Akhil-Tony@users.noreply.github.com>
2021-08-26 11:21:55 +0530

    Update setup.sh
    reverted

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-mkdir -p ~/.streamlitt/
+mkdir -p ~/.streamlit/
 
 echo "\
 [server]\n\
@@ -6,4 +6,4 @@ headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
-" > ~/.streamlitt/config.toml
+" > ~/.streamlit/config.toml
