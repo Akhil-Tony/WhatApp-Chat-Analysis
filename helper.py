edbfd08dadbca4d9f52b0f1f576dcd125812012f
Akhil Tony <72643926+Akhil-Tony@users.noreply.github.com>
2021-08-30 11:28:56 +0530

    Update helper.py
    added raw stop words .. feeling stressed

--- helper.py
+++ helper.py
@@ -92,10 +92,18 @@ def get_wordcloud(df_w):
     text=' '.join(df_w.message.values)
     email_re=r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     # stp_wrds=stopwords.words('english')+['message','deleted']
+    stp_wrds =['message','deleted']+['i','me','my','myself','we','our','ours','ourselves','you',"you're","you've","you'll","you'd",'your','yours','yourself','yourselves','he','him','his','himself','she',
+     "she's",'her','hers','herself','it',"it's",'its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that',"that'll",'these',
+     'those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until',
+     'while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off',
+     'over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor',
+     'not','only','own','same','so','than','too','very','s','t','can','will','just','don',"don't",'should',"should've",'now','d',
+     'll','m','o','re','ve','y','ain','aren',"aren't",'couldn',"couldn't",'didn',"didn't",'doesn',"doesn't",'hadn',"hadn't",'hasn',"hasn't",'haven',"haven't",'isn',"isn't",
+     'ma','mightn',"mightn't",'mustn',"mustn't",'needn',"needn't",'shan',"shan't",'shouldn',"shouldn't",'wasn',"wasn't",'weren',"weren't",'won',"won't",'wouldn',"wouldn't"]
     text= re.sub(email_re,'URL',text)
 
-    cloud=WordCloud(width=3000,height=1700,background_color='black',
-                    max_words=500,min_word_length=4,colormap='Set1').generate(text)   # stopwords=stp_wrds,
+    cloud=WordCloud(width=3000,height=1700,background_color='black',stopwords=stp_wrds,
+                    max_words=500,min_word_length=4,colormap='Set1').generate(text)    
     return cloud
 #Word cloud plotting
 def plott(img):
