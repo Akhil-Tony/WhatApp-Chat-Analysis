3ba0cc480ca5ef8547145d7be13845e6866b8850
Akhil Tony <72643926+Akhil-Tony@users.noreply.github.com>
2021-08-30 11:12:32 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -90,8 +90,8 @@ def get_wordcloud(df_w):
     df_w.dropna(inplace=True)
     df_w=df_w[~(df_w['message']=='<Media omitted>')]
     text=' '.join(df_w.message.values)
-    # email_re=r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
-    # stp_wrds=stopwords.words('english')+['message','deleted']
+    email_re=r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    stp_wrds=stopwords.words('english')+['message','deleted']
     # text= re.sub(email_re,'URL',text)
 
     cloud=WordCloud(width=3000,height=1700,background_color='black',
