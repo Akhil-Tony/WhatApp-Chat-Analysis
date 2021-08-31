3ca8dd48949829e5158e383a051a1d3553ad6260
Akhil Tony <72643926+Akhil-Tony@users.noreply.github.com>
2021-08-31 21:19:37 +0530

    Update helper.py
    updated with classes and objects

--- helper.py
+++ helper.py
@@ -7,7 +7,6 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import plotly.express as px
 import calmap
-from nltk.corpus import stopwords
 import emoji
 import warnings
 warnings.filterwarnings('ignore')
@@ -16,6 +15,15 @@ warnings.filterwarnings('ignore')
 
 d_t_format=['%d/%m/%Y, %I:%M %p','%d/%m/%y, %I:%M %p','%m/%d/%y, %I:%M %p']
 date=re.compile('\d{1,2}/\d{1,2}/\d{2,4}')
+
+y=lambda x:x.year
+emg_extrct=lambda x:''.join(re.findall(emoji.get_emoji_regexp(),x))
+count_w=lambda x:len(x.split())
+count_emoji=lambda x:len(list(x))
+get_date=lambda x:x.date()
+em_name=lambda x:emoji.demojize(x).strip(':')
+get_date=lambda x:x.date()
+get_hr=lambda x:x.hour
 
 def list_to_DF(_list,f=0):
     df=pd.DataFrame(columns=['date_time','author','message'])
@@ -37,17 +45,11 @@ def list_to_DF(_list,f=0):
             df.iloc[-1].message=df.iloc[-1].message+' '+chat
 
     return df
-#******************************************************************************
-y=lambda x:x.year
-emg_extrct=lambda x:''.join(re.findall(emoji.get_emoji_regexp(),x))
-count_w=lambda x:len(x.split())
-count_emoji=lambda x:len(list(x))
-#******************************************************************************
 
 def data_preperation(df):
 
     df.dropna(inplace=True)
-
+    df=df[~(df['message']=='<Media omitted>')]
     df['day']=df['date_time'].apply(pd.Timestamp.day_name)
     df['month']=df['date_time'].apply(pd.Timestamp.month_name)
     df['year']=df['date_time'].apply(y)    #(pd.Timestamp.year)
@@ -57,42 +59,10 @@ def data_preperation(df):
     df['emoji_count']=df.emoji_used.apply(count_emoji)
 
     return df
-#******************************************************************************
-#GRAPHS
 
-# line polar graph
-def linepolar(df):
+class analyse:
 
-    get_date=lambda x:x.date()
-    date=df.date_time.apply(get_date)
-    avg_counts=df.groupby(['day',date],sort=False).size().unstack('date_time').mean(1)
-
-    fig2=px.line_polar(avg_counts,r=avg_counts,theta=avg_counts.index,line_close=True,title='Most Active WeekDays',
-    color_discrete_sequence=px.colors.sequential.Rainbow,template='plotly_dark',height=500,width=500)
-    fig2.update_traces(fill='toself')
-
-    return fig2
-
-# Calender graph
-def calmapp(df):
-    temp=df.copy()
-    temp.date_time=temp.date_time.apply(lambda x:pd.Timestamp(x.date()))
-    temp.set_index('date_time',inplace=True)
-    cal_df=temp.groupby('date_time').size()#['word_count'].sum()  # <-------- why use a .size() here no of texts make sense
-    fg=plt.figure(figsize=(16,10),dpi=80,facecolor='grey')
-    year=max(df['year'])
-    f=calmap.yearplot(cal_df,year=year,monthly_border=True,cmap='terrain_r')
-    fg.colorbar(f.get_children()[1],ax=f,orientation='vertical',aspect=10,shrink=.2)
-
-    return fg
-#Word cloud
-def get_wordcloud(df_w):
-    df_w.dropna(inplace=True)
-    df_w=df_w[~(df_w['message']=='<Media omitted>')]
-    text=' '.join(df_w.message.values)
-    email_re=r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
-    
-    stp_wrds =['message','deleted']+['i','me','my','myself','we','our','ours','ourselves','you',"you're","you've","you'll","you'd",'your','yours','yourself','yourselves','he','him','his','himself','she',
+    stop_words =['message','deleted']+['i','me','my','myself','we','our','ours','ourselves','you',"you're","you've","you'll","you'd",'your','yours','yourself','yourselves','he','him','his','himself','she',
      "she's",'her','hers','herself','it',"it's",'its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that',"that'll",'these',
      'those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until',
      'while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off',
@@ -100,50 +70,90 @@ def get_wordcloud(df_w):
      'not','only','own','same','so','than','too','very','s','t','can','will','just','don',"don't",'should',"should've",'now','d',
      'll','m','o','re','ve','y','ain','aren',"aren't",'couldn',"couldn't",'didn',"didn't",'doesn',"doesn't",'hadn',"hadn't",'hasn',"hasn't",'haven',"haven't",'isn',"isn't",
      'ma','mightn',"mightn't",'mustn',"mustn't",'needn',"needn't",'shan',"shan't",'shouldn',"shouldn't",'wasn',"wasn't",'weren',"weren't",'won',"won't",'wouldn',"wouldn't"]
-    text= re.sub(email_re,'URL',text)
 
-    cloud=WordCloud(width=3000,height=1700,background_color='black',stopwords=stp_wrds,
-                    max_words=500,min_word_length=4,colormap='Set1').generate(text)    
-    return cloud
-#Word cloud plotting
-def plott(img):
-    a=plt.figure(figsize=(20,10))
-    plt.axis('off')
-    plt.imshow(img)
-    return a
+    def __init__(self,chats):
 
-#treemap
-def treemap(df_w):
-    emoji_count=dict(Counter(list(''.join(df_w.emoji_used))))
-    emoji_count=sorted(emoji_count.items(),key=lambda x:x[1],reverse=True)
-    emo_df=pd.DataFrame(emoji_count,columns=['emoji','count'])
-    em_name=lambda x:emoji.demojize(x).strip(':')
-    emo_df['name']=emo_df.emoji.apply(em_name)
+        self.df=data_preperation( list_to_DF(chats) )
 
-    fig=px.treemap(emo_df,path=['emoji'],values=emo_df['count'].tolist(),
-    title='Most Used Emojis in Descending Order',hover_data=['count','name'],template='gridon',
-    height=620,width=850)
-    fig.data[0].hovertemplate='%{label}<br>%{customdata[1]}<br>%{value}'
+    #GRAPHS
 
-    return fig
+    # line polar graph
+    def linepolar(self):
 
-# histogram
-def histogram(df):
-    get_date=lambda x:x.date()
-    get_hr=lambda x:x.hour
-    date=df['date_time'].apply(get_date)
-    time=df['time'].apply(get_hr)
-    active_hours=df.groupby([date,time]).size().unstack('date_time').mean(1)
-    dummy=pd.Series([0]*24)
-    active_hours=np.add(dummy,active_hours).fillna(0).astype(int)
+        date=self.df.date_time.apply(get_date)
+        avg_counts=self.df.groupby(['day',date],sort=False).size().unstack('date_time').mean(1)
 
-    fg=px.histogram(x=active_hours.index,y=active_hours,nbins=24,height=450,width=610,range_x=[0,23],
-    title='Most Active Time Hours',color_discrete_sequence=['violet'])
-    fg.update_xaxes(nticks=28)
-    fg.data[0]['hovertemplate']='Time: %{x}hr<br>%{y} Messages'
-    fg.data[0]['showlegend']=True
-    fg.update_layout(xaxis_title='Time Hour in 24hour Format',
-    yaxis_title='Average Number of Messages',legend_itemclick=False)
-    fg.data[0]['name']='Messages'
+        fig2=px.line_polar(avg_counts,r=avg_counts,theta=avg_counts.index,line_close=True,title='Activity Level Per WeekDay',
+        color_discrete_sequence=px.colors.sequential.Rainbow,template='plotly_dark',height=500,width=500)
+        fig2.update_traces(fill='toself')
 
-    return fg
+        return fig2
+
+    # Calender graph
+    def calmapp(self):
+        temp=self.df.copy()
+        temp.date_time=temp.date_time.apply(lambda x:pd.Timestamp(x.date()))
+        temp.set_index('date_time',inplace=True)
+        cal_df=temp.groupby('date_time').size()#['word_count'].sum()  # <-------- why use a .size() here no of texts make sense
+        fg=plt.figure(figsize=(16,10),dpi=80,facecolor='grey')
+        year=max(self.df['year'])
+        f=calmap.yearplot(cal_df,year=year,monthly_border=True,cmap='terrain_r')
+        fg.colorbar(f.get_children()[1],ax=f,orientation='vertical',aspect=10,shrink=.2)
+
+        return fg
+
+    #Word cloud
+    def get_wordcloud(self):
+
+        text=' '.join(self.df.message.values)
+        email_re=r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+        stp_wrds=self.stop_words
+        text= re.sub(email_re,'URL',text)
+
+        cloud=WordCloud(width=3000,height=1700,stopwords=stp_wrds,background_color='black',
+                        max_words=500,min_word_length=4,colormap='Set1').generate(text)
+        return cloud
+
+    #Word cloud plotting
+    def plott(self,img):
+        a=plt.figure(figsize=(20,10))
+        plt.axis('off')
+        plt.imshow(img)
+        return a
+
+    #treemap
+    def treemap(self):
+
+        emoji_count=dict(Counter(list(''.join(self.df.emoji_used))))
+        emoji_count=sorted(emoji_count.items(),key=lambda x:x[1],reverse=True)
+        emo_df=pd.DataFrame(emoji_count,columns=['emoji','count'])
+
+        emo_df['name']=emo_df.emoji.apply(em_name)
+
+        fig=px.treemap(emo_df,path=['emoji'],values=emo_df['count'].tolist(),
+        title='Emoji TreeMap',hover_data=['count','name'],template='gridon',
+        height=620,width=850)
+        fig.data[0].hovertemplate='%{label}<br>%{customdata[1]}<br>%{value}'
+
+        return fig
+
+    # histogram
+    def histogram(self):
+
+        date=self.df['date_time'].apply(get_date)
+        time=self.df['time'].apply(get_hr)
+        active_hours=self.df.groupby([date,time]).size().unstack('date_time').mean(1)
+        dummy=pd.Series([0]*24)
+        active_hours=np.add(dummy,active_hours).fillna(0).astype(int)
+
+        fg=px.histogram(x=active_hours.index,y=active_hours,nbins=24,height=450,width=610,range_x=[0,23],
+        title='Activity Level Per Time Hour',color_discrete_sequence=['violet'])
+        fg.update_xaxes(nticks=28)
+        fg.data[0]['hovertemplate']='Time: %{x}hr<br>%{y} Messages'
+        fg.data[0]['showlegend']=True
+        fg.update_layout(xaxis_title='Time Hour in 24hour Format',
+        yaxis_title='Average Number of Messages',legend_itemclick=False)
+        fg.data[0]['name']='Messages'
+
+        return fg
+
