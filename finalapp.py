25b35347d09d560004c98c55e79975aa463e3f54
Akhil Tony <72643926+Akhil-Tony@users.noreply.github.com>
2022-01-12 13:54:23 +0530

    Update finalapp.py

--- finalapp.py
+++ finalapp.py
@@ -32,7 +32,7 @@ def students_per_batch(data):
 
     fig = px.histogram(x=hist.index,y=hist,height=450,width=610,
     title="Batch Wise Strength",
-    color_discrete_sequence=['yellow'])
+    color_discrete_sequence=['coral'])
     fig.update_layout(
     xaxis_title="",
     yaxis_title="") 
